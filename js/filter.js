42cec2b2a3ccdabfeff5791add4b5c2d5d62cd5d
Вадим <vadim.tkach1378@gmail.com>
2022-01-12 18:42:04 +0200

    text

--- js/filter.js
+++ js/filter.js
@@ -1,36 +1,36 @@
-document.querySelector('.color-items').onclick = function(e) {
-    let lo;
-    if (e.target.tagName === 'IMG') {
-        lo = e.target.src
-        document.querySelectorAll('.color-items img').forEach(elem =>{
-            if (elem.src === 'http://127.0.0.1:5500/img/color-main.svg') {
-                elem.src = lo;
-            }
-        })
-        lo = e.target.src
-        e.target.src = 'img/color-main.svg'
-    }
-}
+document.querySelector('.color-items').onclick = function (e) {
+  let lo;
+  if (e.target.tagName === 'IMG') {
+    lo = e.target.src;
+    document.querySelectorAll('.color-items img').forEach(elem => {
+      console.log(elem.src.includes('main'));
+      if (elem.src.includes('main')) {
+        elem.src = lo;
+      }
+    });
+    lo = e.target.src;
+    e.target.src = 'img/color-main.svg';
+  }
+};
 
-document.querySelector('.brand-items').onclick = function(e) {
-    let lo;
-    if (e.target.tagName === 'IMG') {
-        lo = e.target.src
-        document.querySelectorAll('.brand-item img').forEach(elem =>{
-            if (elem.src === 'http://127.0.0.1:5500/img/color-brand-main.svg') {
-                elem.src = lo;
-            }
-        })
-        lo = e.target.src
-        e.target.src = 'img/color-brand-main.svg'
-    }
-}
+document.querySelector('.brand-items').onclick = function (e) {
+  let lo;
+  if (e.target.tagName === 'IMG') {
+    lo = e.target.src;
+    document.querySelectorAll('.brand-item img').forEach(elem => {
+      if (elem.src.includes('main')) {
+        elem.src = lo;
+      }
+    });
+    lo = e.target.src;
+    e.target.src = 'img/color-brand-main.svg';
+  }
+};
 
-document.querySelector('.button-filter').onclick = function() {
-    document.querySelector('.filter').style.display = 'flex';
-    
-}
+document.querySelector('.button-filter').onclick = function () {
+  document.querySelector('.filter').style.display = 'flex';
+};
 
-document.querySelector('.filter-but-clean').onclick = function() {
-    document.querySelector('.filter').style.display = 'none';
-}
+document.querySelector('.filter-but-clean').onclick = function () {
+  document.querySelector('.filter').style.display = 'none';
+};
